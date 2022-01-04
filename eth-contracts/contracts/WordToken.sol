c83aee989cbcc4f9d22ae97bb0c4f96cdc47c917
brian <>
2022-01-04 11:52:55 -0500

    initial implementation

--- eth-contracts/contracts/WordToken.sol
+++ eth-contracts/contracts/WordToken.sol
@@ -3,6 +3,40 @@ pragma solidity ^0.8.11;
 import "@rari-capital/solmate/src/tokens/ERC20.sol";
 
 contract WordToken is ERC20 {
+    uint256 public constant PUBLIC_LAUNCH_MINT = 100;
+    uint256 public constant PUBLIC_LAUNCH_MAX = 1000;
+    uint256 public publicLaunchCount;
+
+    mapping(address => bool) public claims;
+
     constructor() ERC20("Wordling Token", "WORD", 18){
+    }
+
+    /**
+     * @dev The initial public launch mint
+     */
+    function publicMint() external {
+        require(publicLaunchCount + PUBLIC_LAUNCH_MINT <= PUBLIC_LAUNCH_MAX, "No claim available");
+        require(!claims[msg.sender], "No claim available");
+        publicLaunchCount += PUBLIC_LAUNCH_MINT;
+        claims[msg.sender] = true;
+        _mint(msg.sender, PUBLIC_LAUNCH_MINT);
+    }
+
+    /**
+     * @dev Giveaway minting
+     */
+    function privateMint() external {
+        // TODO: ecrecover
+        require(!claims[msg.sender], "No claim available");
+        claims[msg.sender] = true;
+        _mint(msg.sender, PUBLIC_LAUNCH_MINT);
+    }
+
+    /**
+     * @dev Reward minting
+     */
+    function rewardMint(address winner, uint256 amount) external {
+        _mint(winner, amount);
     }
 }
---
+++ eth-contracts/contracts/Wordling.sol
@@ -0,0 +1,91 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.11;
+import "./interfaces/IWordToken.sol";
+
+contract Wordling {
+    uint256 public dictionarySize;
+    uint256 cooldown = 180; // number of seconds between fetching words
+    uint256 admission = 100; // number of $WORD tokens that need to be held to play
+
+    address private _adminSigner;
+
+    struct Player {
+        uint256 word; // index of the list of words that is assigned to the user
+        uint256 unclaimed; // number of words attempted since the last reward claim
+        uint256 lastAttempt; // timestamp of the last word retrieved, for rate-limiting
+    }
+    mapping(address => Player) public players;
+
+    // map the number of guesses to scores
+    mapping(uint8 => uint256) public scoreRewards;
+
+    IWordToken wordToken;
+
+    constructor(address wordTokenAddr) {
+        scoreRewards[uint8(1)] = 100;
+        scoreRewards[uint8(2)] = 10;
+        scoreRewards[uint8(3)] = 3;
+        scoreRewards[uint8(4)] = 2;
+        scoreRewards[uint8(5)] = 1;
+        // solidity defaults to 0 so no need:
+        // scoreRewards[uint8(6)] = 0;
+
+        wordToken = IWordToken(wordTokenAddr);
+    }
+
+    /**
+     * @dev Assign a word to the user
+     */
+    function getWord() external {
+        require(admission <= wordToken.balanceOf(msg.sender), "Insufficient $WORD to play");
+
+        Player storage player = players[msg.sender];
+
+        // check that the last assignment was before the cooldown period
+        require(player.lastAttempt < block.timestamp - cooldown, "Cooldown");
+
+        // fetch a word
+        uint256 i = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp))) % dictionarySize;
+
+        // assign the word to the user
+        player.word = i;
+        player.unclaimed += 1;
+        player.lastAttempt = block.timestamp;
+    }
+
+    /**
+     * @dev Free word of the day
+     */
+    function freeWord() external view returns (uint256) {
+        // convert timestamp to day-granularity timestamp
+        return block.timestamp % 86400;
+    }
+
+    /**
+     * @dev calculate rewards based on scores
+     */
+    function calculateReward(uint8[] calldata scores) public view returns (uint256) {
+        uint256 amount;
+        for (uint256 i = 0; i < scores.length; i++){
+            amount += scoreRewards[scores[i]];
+        }
+        return amount;
+    }
+
+    /**
+     * @dev Claim your wins
+     */
+    function mintWins(bytes32 _hash, bytes32 _r, bytes32 _s, uint8 _v, uint8[] calldata scores) external {
+        address signer = ecrecover(_hash, _v, _r, _s);
+        require(signer == _adminSigner, "Forged signature");
+        // TODO invalidate signature
+
+        Player storage player = players[msg.sender];
+        require(player.unclaimed != 0, "Incorrect scores");
+        require(player.unclaimed == scores.length, "Incorrect scores");
+
+        player.unclaimed = 0;
+        uint256 reward = calculateReward(scores);
+        wordToken.rewardMint(msg.sender, reward);
+    }
+}
